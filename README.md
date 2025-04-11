f475b3c3c0735ec5ae15438b6b6e0ee6748fc34d
Knowledgetodiwarld <fhmomer@gmail.com>
2025-04-11 17:45:27 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Evolve Beyond Religion
+
+A philosophical project and web platform to inspire critical thinking and a secular evolution of consciousness.
---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EvolveBeyondReligion from './EvolveBeyondReligion'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EvolveBeyondReligion />
+  </React.StrictMode>,
+)
