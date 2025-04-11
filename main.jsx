c330d690fb70500882acb36058ea37debbc3fa41
Knowledgetodiwarld <fhmomer@gmail.com>
2025-04-11 18:21:08 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import EvolveBeyondReligion from './EvolveBeyondReligion'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <EvolveBeyondReligion />
-  </React.StrictMode>,
-)
